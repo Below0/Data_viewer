41b1558956dd6f05b7922efa6b84a1ecfc74896e
HAYOUNG LEE <l4538@naver.com>
2020-02-05 21:52:33 +0900

    add DB and Service

---
+++ app/src/main/java/com/example/datachecker/DBHelper.java
@@ -0,0 +1,72 @@
+package com.example.datachecker;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+
+    // DBHelper 생성자로 관리할 DB 이름과 버전 정보를 받음
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    // DB를 새로 생성할 때 호출되는 함수
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE server_data (_id INTEGER PRIMARY KEY AUTOINCREMENT, value INTEGER, create_at date default (datetime('now','+9 hours')));");
+        Log.d("db","db_create");
+    }
+
+    // DB 업그레이드를 위해 버전이 변경될 때 호출되는 함수
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void insert(int value) {
+        // 읽고 쓰기가 가능하게 DB 열기
+        SQLiteDatabase db = getWritableDatabase();
+        // DB에 입력한 값으로 행 추가
+        db.execSQL("INSERT INTO server_data(value) VALUES('"+ value + "');");
+        db.close();
+    }
+
+    public void update(String item, int price) {
+        SQLiteDatabase db = getWritableDatabase();
+        // 입력한 항목과 일치하는 행의 가격 정보 수정
+        db.execSQL("UPDATE MONEYBOOK SET price=" + price + " WHERE item='" + item + "';");
+        db.close();
+    }
+
+    public void delete(String item) {
+        SQLiteDatabase db = getWritableDatabase();
+        // 입력한 항목과 일치하는 행 삭제
+        db.execSQL("DELETE FROM server_data WHERE item='" + item + "';");
+        db.close();
+    }
+
+    public String getResult() {
+        // 읽기가 가능하게 DB 열기
+        SQLiteDatabase db = getReadableDatabase();
+        String result = "";
+
+        // DB에 있는 데이터를 쉽게 처리하기 위해 Cursor를 사용하여 테이블에 있는 모든 데이터 출력
+        Cursor cursor = db.rawQuery("SELECT * FROM server_data ORDER BY create_at desc limit 3 ", null);
+        while (cursor.moveToNext()) {
+            result += cursor.getInt(0)
+                    + " : "
+                    + cursor.getInt(1)
+                    + " | "
+                    + cursor.getString(2);
+            Log.d("db",result);
+
+        }
+
+        return result;
+    }
+}
+
